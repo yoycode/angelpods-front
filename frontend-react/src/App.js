224812640d1c3c0fca18d6b59b06d643e9b2fab0
jiyeon <jy7828@gmail.com>
2022-02-19 13:56:01 +0900

    API test

--- frontend-react/src/App.js
+++ frontend-react/src/App.js
@@ -1,12 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import { textSpanContainsPosition } from 'typescript';
+import { EventEmitter } from 'stream';
 
 function App() {
   return (
     <div className="App">
-      Hello World2
+      <button className="btn btn-primary" onClick={() => {
+        axios.post('localhost:8080/api/find')
+          .then((res) => {
+            console.log(res)
+          })
+          .catch((err) => {
+            console.error(err)
+          })
+      }}>API통신</button>
     </div>
   );
 }
+
 
 export default App;

50c100ddfc0525a41a92c7b3db69e9f4471b5949
woajun <woajun@naver.com>
2022-03-09 23:50:14 +0900

    ＃１２　롬복　의존성　제거

--- backend/src/main/java/me/juni/angelpods/find/Find.java
+++ backend/src/main/java/me/juni/angelpods/find/Find.java
@@ -6,20 +6,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
 @Entity
-@EqualsAndHashCode(of = "id")
-@Getter 
-@Setter 
-@Builder 
-@NoArgsConstructor 
-@AllArgsConstructor
 public class Find {
 
 	@Id @GeneratedValue
@@ -36,5 +23,85 @@ public class Find {
 	private String phone;
 	private LocalDateTime createdAt;
 	private LocalDateTime lastUpdatedAt;
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getmCategory() {
+		return mCategory;
+	}
+	public void setmCategory(String mCategory) {
+		this.mCategory = mCategory;
+	}
+	public String getsCategory() {
+		return sCategory;
+	}
+	public void setsCategory(String sCategory) {
+		this.sCategory = sCategory;
+	}
+	public String getLat() {
+		return lat;
+	}
+	public void setLat(String lat) {
+		this.lat = lat;
+	}
+	public String getLng() {
+		return lng;
+	}
+	public void setLng(String lng) {
+		this.lng = lng;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public String getiName() {
+		return iName;
+	}
+	public void setiName(String iName) {
+		this.iName = iName;
+	}
+	public LocalDateTime getGetTime() {
+		return getTime;
+	}
+	public void setGetTime(LocalDateTime getTime) {
+		this.getTime = getTime;
+	}
+	public String getGetLoc() {
+		return getLoc;
+	}
+	public void setGetLoc(String getLoc) {
+		this.getLoc = getLoc;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	public LocalDateTime getCreatedAt() {
+		return createdAt;
+	}
+	public void setCreatedAt(LocalDateTime createdAt) {
+		this.createdAt = createdAt;
+	}
+	public LocalDateTime getLastUpdatedAt() {
+		return lastUpdatedAt;
+	}
+	public void setLastUpdatedAt(LocalDateTime lastUpdatedAt) {
+		this.lastUpdatedAt = lastUpdatedAt;
+	}
+	
 	
 }

0d255f67076e935b8e3b17df41044bff113fc362
jiyeon <jy7828@gmail.com>
2022-03-10 23:57:42 +0900

    #14  전체적인 레이아웃 잡기

--- frontend-react/src/App.js
+++ frontend-react/src/App.js
@@ -1,21 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import './css/App.css';
 import axios from 'axios';
-import { textSpanContainsPosition } from 'typescript';
-import { EventEmitter } from 'stream';
 
 function App() {
   return (
     <div className="App">
-      <button className="btn btn-primary" onClick={() => {
-        axios.post('http://localhost:5000/api/find')
+      {/* <button className="btn btn-primary" onClick={() => {
+        axios.post('localhost:8080/api/find')
           .then((res) => {
             console.log(res)
           })
           .catch((err) => {
             console.error(err)
           })
-      }}>API통신</button>
+      }}>API통신</button> */}
     </div>
   );
 }
